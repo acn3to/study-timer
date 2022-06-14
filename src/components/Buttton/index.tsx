0e8f684f4fe2d1464fe0ef39f7c6ea357edd8136
Arnaldo Neto <costanetoow@gmail.com>
2022-06-14 13:05:35 -0300

    style: add button style

--- src/components/Buttton/index.tsx
+++ src/components/Buttton/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './style.scss';
 
 class Button extends React.Component {
   render() {
-    return <button>Button</button>;
+    return <button className="button">Button</button>;
   }
 }
 
