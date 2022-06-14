b7dc9954d7038746ec55adbac7d95dad8febbd0a
Arnaldo Neto <costanetoow@gmail.com>
2022-06-14 13:06:32 -0300

    style: add list style

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.scss';
 
 function List() {
   const tasks = [
@@ -16,11 +17,11 @@ function List() {
     },
   ];
   return (
-    <aside>
+    <aside className="tasksList">
       <h2>Today studies</h2>
       <ul>
         {tasks.map((item, index) => (
-          <li key={index}>
+          <li key={index} className="item">
             <h3>{item.task}</h3>
             <span>{item.time}</span>
           </li>
