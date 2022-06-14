8df47b8011d0f3dd457123789130fd067020e484
Arnaldo Neto <costanetoow@gmail.com>
2022-06-14 15:58:03 -0300

    refactor: separate list functionalities using props

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item';
 import style from './List.module.scss';
 
 function List() {
@@ -21,10 +22,7 @@ function List() {
       <h2>Today studies</h2>
       <ul>
         {tasks.map((item, index) => (
-          <li key={index} className={style.item}>
-            <h3>{item.task}</h3>
-            <span>{item.time}</span>
-          </li>
+          <Item key={index} {...item} />
         ))}
       </ul>
     </aside>
