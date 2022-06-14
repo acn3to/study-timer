221df3c5729216724a26d8daa684523fa853583c
Arnaldo Neto <costanetoow@gmail.com>
2022-06-14 15:43:49 -0300

    feat: add props to button

--- src/components/Buttton/index.tsx
+++ src/components/Buttton/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import style from './Button.module.scss';
 
-class Button extends React.Component {
+class Button extends React.Component<{ children: any }> {
   render() {
-    return <button className={style.button}>Button</button>;
+    return <button className={style.button}>{this.props.children}</button>;
   }
 }
 
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -28,7 +28,7 @@ class Form extends React.Component {
             required
           />
         </div>
-        <Button />
+        <Button>Add</Button>
       </form>
     );
   }
