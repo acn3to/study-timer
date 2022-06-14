56a56a651ecc4f68ad38214ad215f77c6557f888
Arnaldo Neto <costanetoow@gmail.com>
2022-06-14 12:24:02 -0300

    chore: create pages folder for a better organization

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from '../components/Form';
+import List from '../components/List';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Form />
+      <List />
+    </div>
+  );
+}
+
+export default App;
